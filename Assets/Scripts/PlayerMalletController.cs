0204be3c0304919202cb3858fb7c99f5a7088a4e
dzung <mu7991@gmail.com>
2014-06-16 11:00:55 +0700

    first commit

---
+++ Assets/Scripts/PlayerMalletController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMalletController : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		Vector3 var = Vector3.zero;
+		var.x = Input.GetAxis("Horizontal");
+		var.z = Input.GetAxis("Vertical");
+		transform.Translate(var*Time.deltaTime*20.0f);
+		                
+	}
+}
---
+++ Assets/Scripts/PuckController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuckController : MonoBehaviour {
+
+	public GameObject PlayerMallet;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		if( col.gameObject.name == "Mallet_player")
+		{
+			Vector3 vector_diff = gameObject.transform.position - PlayerMallet.transform.position;
+			rigidbody.AddForce((vector_diff/vector_diff.magnitude)*1000);
+		}
+	}
+}
