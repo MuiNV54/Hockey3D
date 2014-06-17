be8f14c94ccb5f1465e3fa4b061cf6a9e71658c4
dzung <mu7991@gmail.com>
2014-06-17 09:13:04 +0700

    Physics Added

--- Assets/Scripts/PuckController.cs
+++ Assets/Scripts/PuckController.cs
@@ -11,14 +11,18 @@ public class PuckController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
+		Vector3 vector_diff = gameObject.transform.position - PlayerMallet.transform.position;
+		if (vector_diff.magnitude <
+		    (gameObject.transform.localScale.x + PlayerMallet.gameObject.transform.localScale.x)/2)
+			rigidbody.AddForce((vector_diff/vector_diff.magnitude)*300);
 	}
 	
-	void OnCollisionEnter(Collision col) {
-		if( col.gameObject.name == "Mallet_player")
-		{
-			Vector3 vector_diff = gameObject.transform.position - PlayerMallet.transform.position;
-			rigidbody.AddForce((vector_diff/vector_diff.magnitude)*1000);
-		}
-	}
+//	void OnCollisionEnter(Collision col) {
+//		if( col.gameObject.name == "Mallet_player")
+//		{
+//			Vector3 vector_diff = gameObject.transform.position - PlayerMallet.transform.position;
+//			rigidbody.AddForce((vector_diff/vector_diff.magnitude)*500);
+//		}
+//	}
+
 }
