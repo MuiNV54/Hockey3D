d6850daa686459036cb3d66c949e3ca7b295eab5
MuiNV54 <nguyen.van.mui@framgia.com>
2014-06-18 15:21:30 +0700

    Add Textures

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovement : MonoBehaviour 
+{
+	private Vector3 initPosition;
+	public GameObject Puck;
+
+	void Start () 
+	{
+		initPosition = transform.position;
+	}
+	
+	void Update () 
+	{
+		transform.LookAt (Puck.transform.position);
+		transform.position = Vector3.Lerp (transform.position, initPosition, 5 * Time.deltaTime);
+		AIBehavior ();
+	}
+
+	void AIBehavior()
+	{
+		if (Puck.transform.position.z >= 0)
+		{
+			rigidbody.AddForce(transform.forward * 150000);
+			Debug.Log("ABC123");
+		}
+	}
+}
